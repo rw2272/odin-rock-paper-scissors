15df9e90c4a4d546cbf2fcdda7c882859a1773d5
Raymond <RW2272@gmail.com>
2023-02-23 23:30:26 -0600

    20230223 final project upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-
-//getComputerChoice()
-
 function getComputerChoice(n){
     computerChoice = Math.ceil(Math.random() * 3)
     switch(computerChoice) {
@@ -21,45 +18,22 @@ function getComputerChoice(n){
     return computerChoice
 }
 
-//playerSelection()
-
-function playerSelection(n){
-    var playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
-    console.log(playerChoice)
-    switch(playerChoice) {
-        case "rock":
-            playerChoice = "rock"
-            break;
-        case "paper":
-            playerChoice = "paper"
-            break;
-        case "scissors":
-            playerChoice = "scissors"
-            break;
-        default:
-            playerChoice = "broken"
-            break;
-      }
-    console.log(`you have chosen ${playerChoice}`)
-    return playerChoice
-}
-
-function playerSelectionV2(n){
-    console.log("hello world")
-}
-
-function playRound(playerSelectionV2, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     if((playerSelection == 'rock' && computerSelection == 'rock') || (playerSelection == 'paper' && computerSelection == 'paper') || (playerSelection == 'scissors' && computerSelection == 'scissors')){
         console.log('Tie')
+        document.querySelector("#gameresults").innerHTML = "RESULT: TIE"
+        scoreTies++
         return 'Tie'
     }
     if((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')){
         console.log('You Win')
+        document.querySelector("#gameresults").innerHTML = "RESULT: YOU WIN"
         scorePlayer++
         return 'You Win'
     }
     if((playerSelection == 'rock' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'scissors') || (playerSelection == 'scissors' && computerSelection == 'rock')){
         console.log('CPU wins')
+        document.querySelector("#gameresults").innerHTML = "RESULT: CPU WINS"
         scoreCPU++
         return 'CPU wins'
     }
@@ -70,29 +44,63 @@ function playRound(playerSelectionV2, computerSelection) {
   }
 
 function game(){
-    for (let i = 0; i < 5; i++) {
-        playRound(playerSelectionV2(), getComputerChoice());
-        console.log(`CPU score: ${scoreCPU}`)
-        console.log(`Player score: ${scorePlayer}`)
-     } 
+    playRound(playerChoice, getComputerChoice());
+    console.log(`CPU score: ${scoreCPU}`)
+    console.log(`Player score: ${scorePlayer}`)
+    updateplayerchoice()
+    updatecomputerchoice()
 }
 
+function rockbuttonfunction(){
+    console.log('rockbuttonfunction clicked')
+    playerChoice = 'rock'
+    game()
+}
+
+function paperbuttonfunction(){
+    console.log('paperbuttonfunctionclicked')
+    playerChoice = 'paper'
+    game()
+}
+
+function scissorsbuttonfunction(){
+    console.log('scissorsbuttonfunction clicked')
+    playerChoice = 'scissors'
+    game()
+}
+
+function updateplayerchoice(){
+    choicetitlescore++
+    document.querySelector("#playerchoice").innerHTML = `You have chosen: ${playerChoice}`
+    document.querySelector("#choicetitle").innerHTML = `ROUND ${choicetitlescore}`
+    document.querySelector("#totalscoreboard").innerHTML = `CPU: ${scoreCPU} Player: ${scorePlayer} Ties: ${scoreTies}`
+}
+
+function updatecomputerchoice(){
+    document.querySelector("#computerchoice").innerHTML = `The computer chooses: ${computerChoice}`
+ }
+
+ function resetfunction(){
+    choicetitlescore = 0
+    scoreCPU = 0
+    scorePlayer = 0
+    scoreTies = 0
+    document.querySelector("#choicetitle").innerHTML = `ROUND ${choicetitlescore}`
+    document.querySelector("#totalscoreboard").innerHTML = `CPU: ${scoreCPU} Player: ${scorePlayer} Ties: ${scoreTies}`
+    document.querySelector("#gameresults").innerHTML = ""
+    document.querySelector("#playerchoice").innerHTML = ""
+    document.querySelector("#computerchoice").innerHTML = ""
+ }
+
+
+choicetitlescore = 0
 scoreCPU = 0
 scorePlayer = 0
-game()
+scoreTies = 0
+
 
 document.querySelector("#rockbutton").addEventListener("click", rockbuttonfunction);
 document.querySelector("#paperbutton").addEventListener("click", paperbuttonfunction);
 document.querySelector("#scissorsbutton").addEventListener("click", scissorsbuttonfunction);
 
-function rockbuttonfunction(){
-    console.log('rockbuttonfunction clicked')
-}
-
-function paperbuttonfunction(){
-    console.log('paperbuttonfunctionclicked')
-}
-
-function scissorsbuttonfunction(){
-    console.log('scissorsbuttonfunction clicked')
-}
+document.querySelector("#resetbutton").addEventListener("click", resetfunction);
